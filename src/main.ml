2fdc641d3983a8967c2c78a3032ac93e0d98013e
iKazai <91919794+iKazai@users.noreply.github.com>
2025-12-19 17:44:47 +0100

    ajout de la lecture dans le main

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,51 @@
-let () = print_endline "Hello, World!"
+(* Chargement d'un plan de base martienne et traduction en graphe dynamique. *)
+
+module G = Dyngraph.Make(Dyngraph.Vertex)
+
+(* Construit le graphe à partir des transitions lues. *)
+let build_graph transitions =
+	let id_counter = ref 0 in
+	let name_to_vertex = Hashtbl.create 16 in
+
+	let get_vertex name =
+		match Hashtbl.find_opt name_to_vertex name with
+		| Some v -> v
+		| None ->
+				incr id_counter;
+				let v = Dyngraph.Vertex.make name !id_counter in
+				Hashtbl.add name_to_vertex name v;
+				v
+	in
+
+	let graph =
+		List.fold_left
+			(fun g (src, dst, w) ->
+				 let v_src = get_vertex src in
+				 let v_dst = get_vertex dst in
+				 let g = G.add_edge v_src w v_dst g in
+				 let g = G.add_edge v_dst w v_src g in
+				 g)
+			G.empty
+			transitions
+	in
+	graph, name_to_vertex
+
+let print_graph g =
+	Hashtbl.iter
+		(fun name neighbors ->
+			 Printf.printf "%s : %s\n" name
+				 (neighbors
+					|> List.map (fun (v, w) ->
+							Printf.sprintf "--%d--> %s" w (Dyngraph.Vertex.get_name v))
+					|> String.concat ", "))
+		g
+
+let () =
+	print_endline "Nom du fichier de base martienne ?";
+	let file = read_line () in
+	let transitions, (start_name, goal_name) = Analyse.analyse_file_1 file in
+	let graph, table = build_graph transitions in
+	Printf.printf "Plan charge : %d tunnels, %d modules.\n"
+		(List.length transitions) (Hashtbl.length table);
+	Printf.printf "Depart : %s | Arrivee : %s\n" start_name goal_name;
+	print_endline "Graphe (aretes bidirectionnelles) :";
--- test/test_best_path.ml
+++ test/test_best_path.ml
@@ -11,6 +11,11 @@ let test_djikstra () =
   let g = G.add_vertex v3 g in
   let g = G.add_edge v1 1 v2 g in
   let g = G.add_edge v2 2 v3 g in
+  let g = G.add_edge v1 4 v3 g in
+  assert (G.dijkstra v1 v3 g = Some 3);
+  assert (G.dijkstra v1 v2 g = Some 1);
+  assert (G.dijkstra v2 v3 g = Some 2);
+  assert (G.dijkstra v1 v1 g = Some 0);
   Printf.printf "Dijkstra test passed!\n"
 
 let () =
