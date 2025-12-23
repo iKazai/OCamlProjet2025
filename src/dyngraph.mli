1f00f55f9d558a02b0c149d5f3c52e8ac01bbffe
iKazai <91919794+iKazai@users.noreply.github.com>
2025-12-23 03:01:05 +0100

    commentaire pour dijkstra

--- src/dyngraph.mli
+++ src/dyngraph.mli
@@ -7,6 +7,14 @@ module type DYNAMIC_GRAPH = sig
     val remove_vertex : vertex -> graph -> graph
     val add_edge : vertex -> int -> vertex -> graph -> graph
     val remove_edge : vertex -> int -> vertex -> graph -> graph
+
+    (*
+    [dijkstra graph source] retourne une liste de couples (v,(d,p)) où v est un sommet atteignable depuis source, d est la distance minimale entre source et v, et p est le chemin minimal entre source et v (sous forme de liste de sommets, source en tête, v en queue).
+    Si un sommet n'est pas atteignable depuis source, il n'apparaît pas dans la liste.
+    @requires source est un sommet de graph
+    @ensures tous les sommets de la liste sont atteignables depuis source dans graph
+    @ensures tous les sommets atteignables depuis source dans graph apparaissent dans la liste
+    *)
     val dijkstra : graph -> vertex -> (vertex * (int * vertex list)) list
 end
 
